d485902b25eb96d2a9e6b0af9545bc7094c6dad1
Yevgen Lirnyk <Yevgen.Lirnyk@ihsmarkit.com>
2019-11-01 18:50:52 +0200

    [TFX-1594] Code cleanup

--- src/main/java/com/ihsmarkit/tfx/eod/batch/MarkToMarketTradesTasklet.java
+++ src/main/java/com/ihsmarkit/tfx/eod/batch/MarkToMarketTradesTasklet.java
@@ -2,15 +2,12 @@ package com.ihsmarkit.tfx.eod.batch;
 
 import static com.ihsmarkit.tfx.eod.config.SpringBatchConfig.BUSINESS_DATE_FMT;
 
-import com.ihsmarkit.tfx.core.dl.entity.*;
-import com.ihsmarkit.tfx.core.dl.entity.eod.EodProductCashSettlementEntity;
-import com.ihsmarkit.tfx.core.dl.entity.eod.ParticipantPositionEntity;
-import com.ihsmarkit.tfx.core.dl.repository.eod.EodProductCashSettlementRepository;
-import com.ihsmarkit.tfx.core.dl.repository.eod.ParticipantPositionRepository;
-import com.ihsmarkit.tfx.core.domain.type.EodProductCashSettlementType;
-import com.ihsmarkit.tfx.core.domain.type.ParticipantPositionType;
-import com.ihsmarkit.tfx.eod.mtm.DailySettlementPriceRegistry;
-import com.ihsmarkit.tfx.eod.mtm.TradeMTM;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Stream;
+
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
@@ -20,16 +17,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.ihsmarkit.tfx.core.dl.entity.AmountEntity;
+import com.ihsmarkit.tfx.core.dl.entity.CurrencyPairEntity;
+import com.ihsmarkit.tfx.core.dl.entity.TradeEntity;
+import com.ihsmarkit.tfx.core.dl.entity.eod.EodProductCashSettlementEntity;
+import com.ihsmarkit.tfx.core.dl.entity.eod.ParticipantPositionEntity;
 import com.ihsmarkit.tfx.core.dl.repository.TradeRepository;
+import com.ihsmarkit.tfx.core.dl.repository.eod.EodProductCashSettlementRepository;
+import com.ihsmarkit.tfx.core.dl.repository.eod.ParticipantPositionRepository;
+import com.ihsmarkit.tfx.core.domain.type.EodProductCashSettlementType;
+import com.ihsmarkit.tfx.core.domain.type.ParticipantPositionType;
+import com.ihsmarkit.tfx.eod.mtm.DailySettlementPriceRegistry;
+import com.ihsmarkit.tfx.eod.mtm.TradeMTM;
 
 import lombok.RequiredArgsConstructor;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.Collection;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 @RequiredArgsConstructor
@@ -63,14 +64,14 @@ public class MarkToMarketTradesTasklet implements Tasklet {
 
         Stream<TradeEntity> novatedTrades = tradeRepository.findAllNovatedForTradeDate(businessDate);
         final Stream<EodProductCashSettlementEntity> initial = dsp.calculateAndAggregateInitialMTM(novatedTrades)
-                .map((a) -> mapToEodProductCashSettlementEntity(a, businessDate, EodProductCashSettlementType.INITIAL_MTM));
+                .map(a -> mapToEodProductCashSettlementEntity(a, businessDate, EodProductCashSettlementType.INITIAL_MTM));
         eodProductCashSettlementRepository.saveAll(initial::iterator);
 
         Collection<ParticipantPositionEntity> positions =
                 participantPositionRepository.findAllByPositionTypeAndTradeDateFetchCurrencyPair(ParticipantPositionType.SOD, businessDate);
 
         final Stream<EodProductCashSettlementEntity> daily = dsp.calculateAndAggregateDailyMTM(positions)
-                .map((a) -> mapToEodProductCashSettlementEntity(a, businessDate, EodProductCashSettlementType.DAILY_MTM));
+                .map(a -> mapToEodProductCashSettlementEntity(a, businessDate, EodProductCashSettlementType.DAILY_MTM));
 
         eodProductCashSettlementRepository.saveAll(daily::iterator);
 
