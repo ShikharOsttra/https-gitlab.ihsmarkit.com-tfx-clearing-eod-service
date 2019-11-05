7d56209262076cdc67dd7bcca68a32707ba7ce14
Yevgen Lirnyk <Yevgen.Lirnyk@ihsmarkit.com>
2019-11-05 17:14:50 +0200

    [TFX-1594] Code cleanup

--- src/main/java/com/ihsmarkit/tfx/eod/batch/MarkToMarketTradesTasklet.java
+++ src/main/java/com/ihsmarkit/tfx/eod/batch/MarkToMarketTradesTasklet.java
@@ -51,7 +51,7 @@ public class MarkToMarketTradesTasklet implements Tasklet {
     private final MarkToMarketTradeMapper mtmMapper;
 
     @Value("#{jobParameters['businessDate']}")
-    private String businessDateJobParameter;
+    private final String businessDateJobParameter;
 
     @Override
     public RepeatStatus execute(final StepContribution contribution, final ChunkContext chunkContext) {
