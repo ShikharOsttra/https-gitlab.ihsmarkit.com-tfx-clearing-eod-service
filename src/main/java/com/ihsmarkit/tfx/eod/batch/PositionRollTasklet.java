059a154a5753df94017f65dfae314670c88266b9
Yevgen Lirnyk <Yevgen.Lirnyk@ihsmarkit.com>
2019-11-26 15:15:17 +0200

    [TFX-556] basic implementation of position roll

--- src/main/java/com/ihsmarkit/tfx/eod/batch/PositionRollTasklet.java
+++ src/main/java/com/ihsmarkit/tfx/eod/batch/PositionRollTasklet.java
@@ -1,6 +1,9 @@
 package com.ihsmarkit.tfx.eod.batch;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobScope;
@@ -10,7 +13,13 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.ihsmarkit.tfx.core.dl.entity.CurrencyPairEntity;
+import com.ihsmarkit.tfx.core.dl.entity.eod.ParticipantPositionEntity;
 import com.ihsmarkit.tfx.core.dl.repository.eod.ParticipantPositionRepository;
+import com.ihsmarkit.tfx.core.domain.type.ParticipantPositionType;
+import com.ihsmarkit.tfx.eod.mapper.ParticipantPositionForPairMapper;
+import com.ihsmarkit.tfx.eod.service.DailySettlementPriceProvider;
+import com.ihsmarkit.tfx.eod.service.EODCalculator;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,11 +30,34 @@ public class PositionRollTasklet implements Tasklet {
 
     private final ParticipantPositionRepository participantPositionRepository;
 
+    private final EODCalculator eodCalculator;
+
+    private final ParticipantPositionForPairMapper participantPositionForPairMapper;
+
+
+    private final DailySettlementPriceProvider dailySettlementPriceProvider;
+
     @Value("#{jobParameters['businessDate']}")
     private final LocalDate businessDate;
 
     @Override
     public RepeatStatus execute(final StepContribution contribution, final ChunkContext chunkContext) throws Exception {
+
+        final Map<CurrencyPairEntity, BigDecimal> dsp = dailySettlementPriceProvider.getDailySettlementPrices(businessDate);
+
+        Stream<ParticipantPositionEntity> positions = participantPositionRepository.findAllNetAndRebalancingPositionsByTradeDate(businessDate);
+        Stream<ParticipantPositionEntity> aggregated = eodCalculator.aggregatePositions(positions)
+            .map(
+                position -> participantPositionForPairMapper.toParticipantPosition(
+                    position,
+                    ParticipantPositionType.SOD,
+                    businessDate.plusDays(1),
+                    businessDate.plusDays(4), //FIXME: settlement date by ccy?
+                    dsp.get(position.getCurrencyPair())
+                )
+            );
+
+        participantPositionRepository.saveAll(aggregated::iterator);
 
         return RepeatStatus.FINISHED;
     }
