40d1bfbe011750e6fa337eb969ddce8902f06352
Andrii Korovin <andrii.korovin@ihsmarkit.com>
2019-12-27 16:22:44 +0200

    [TFX-2025] Fix spring batch config

--- src/main/java/com/ihsmarkit/tfx/eod/config/SpringBatchConfig.java
+++ src/main/java/com/ihsmarkit/tfx/eod/config/SpringBatchConfig.java
@@ -1,9 +1,21 @@
 package com.ihsmarkit.tfx.eod.config;
 
+import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.support.JobRegistryBeanPostProcessor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableBatchProcessing
 public class SpringBatchConfig {
+
+    @Bean
+    public JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor(final JobRegistry jobRegistry) {
+        final JobRegistryBeanPostProcessor jobRegistryBeanPostProcessor = new JobRegistryBeanPostProcessor();
+        jobRegistryBeanPostProcessor.setJobRegistry(jobRegistry);
+
+        return jobRegistryBeanPostProcessor;
+    }
+
 }
