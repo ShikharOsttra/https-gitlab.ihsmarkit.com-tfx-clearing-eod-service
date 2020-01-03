454c573d90cd6e53679faeaa1657ac0e45633df9
Yevgen Lirnyk <Yevgen.Lirnyk@ihsmarkit.com>
2020-01-03 18:42:52 +0200

    [TFX-558] Spring statemachine based triggers

---
+++ src/main/java/com/ihsmarkit/tfx/eod/statemachine/StateMachineConfig.java
@@ -0,0 +1,262 @@
+package com.ihsmarkit.tfx.eod.statemachine;
+
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.DATE_ROLL_RUN;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.DSP_CHECK;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.DSP_NO_TRADES;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.DSP_NO_TRADES_DELAY;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.EOD1;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.EOD1_COMPLETE;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.EOD1_READY;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.EOD1_RUN;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.EOD2;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.EOD2_COMPLETE;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.EOD2_RUN;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.IDLE;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.LEDGER_COMPLETE;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.LEDGER_RUN;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.NO_DSP_NO_TRADES;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.NO_DSP_NO_TRADES_DELAY;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.NO_DSP_TRADES;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.NO_DSP_TRADES_DELAY;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.SWP_PNT_APPROVED;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.SWP_PNT_CHECK;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.SWP_PNT_DELAY;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.SWP_PNT_NOTAPPROVED;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.statemachine.StateContext;
+import org.springframework.statemachine.action.Action;
+import org.springframework.statemachine.config.EnableStateMachine;
+import org.springframework.statemachine.config.EnumStateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
+import org.springframework.statemachine.guard.Guard;
+
+@EnableStateMachine
+public class StateMachineConfig extends EnumStateMachineConfigurerAdapter<StateMachineConfig.States, StateMachineConfig.Events> {
+    public enum States {IDLE, EOD1, NO_DSP_NO_TRADES, NO_DSP_NO_TRADES_DELAY, DSP_CHECK, DSP_NO_TRADES, DSP_NO_TRADES_DELAY, NO_DSP_TRADES,
+        NO_DSP_TRADES_DELAY, EOD1_READY, EOD1_RUN, EOD1_COMPLETE, EOD2, SWP_PNT_CHECK, SWP_PNT_NOTAPPROVED, SWP_PNT_APPROVED,
+        SWP_PNT_DELAY, EOD2_RUN, EOD2_COMPLETE, LEDGER_RUN, LEDGER_COMPLETE, DATE_ROLL_RUN
+    }
+    public enum Events {EOD}
+
+    private static final int WAIT_TIME = 1000;
+
+    @Autowired
+    @Qualifier("dspApprovedGuard")
+    private Guard<States, Events> dspApprovedGuard;
+
+    @Autowired
+    @Qualifier("tradesInFlightGuard")
+    private Guard<States, Events> tradesInFlightGuard;
+
+    @Autowired
+    @Qualifier("swpPointApprovedGuard")
+    private Guard<States, Events> swpPointApprovedGuard;
+
+    @Autowired
+    @Qualifier("eod1runAction")
+    private Action<States, Events> eod1runAction;
+
+    @Autowired
+    @Qualifier("eod1CompleteAction")
+    private Action<States, Events> eod1CompleteAction;
+
+    @Autowired
+    @Qualifier("eod2runAction")
+    private Action<States, Events> eod2runAction;
+
+    @Autowired
+    @Qualifier("ledgerRunAction")
+    private Action<States, Events> ledgerRunAction;
+
+    @Autowired
+    @Qualifier("dateRollRunAction")
+    private Action<States, Events> dateRollRunAction;
+
+    @Override
+    public void configure(StateMachineStateConfigurer<States, Events> states) throws Exception {
+        states.
+            withStates()
+            .initial(IDLE)
+            .choice(EOD1_RUN)
+            .choice(EOD2_RUN)
+            .choice(LEDGER_RUN)
+            .states(Set.of(EOD1, EOD1_COMPLETE, EOD2, EOD2_COMPLETE, LEDGER_COMPLETE, DATE_ROLL_RUN))
+            .and()
+                .withStates()
+                .parent(EOD1)
+                .initial(DSP_CHECK)
+                .end(EOD1_READY)
+                .choice(NO_DSP_NO_TRADES)
+                .choice(DSP_NO_TRADES)
+                .choice(NO_DSP_TRADES)
+                .states(Set.of(NO_DSP_TRADES_DELAY, DSP_NO_TRADES_DELAY, NO_DSP_NO_TRADES_DELAY))
+            .and()
+                .withStates()
+                .parent(EOD2)
+                .initial(SWP_PNT_CHECK)
+                .choice(SWP_PNT_NOTAPPROVED)
+                .state(SWP_PNT_DELAY)
+                .end(SWP_PNT_APPROVED)
+            ;
+    }
+
+    @Override
+    public void configure(StateMachineTransitionConfigurer<States, Events> transitions) throws Exception {
+        transitions
+            .withExternal()
+                .source(DSP_CHECK).target(NO_DSP_NO_TRADES)
+            .and().withExternal()
+                .source(IDLE).target(EOD1).event(Events.EOD)
+            .and().withChoice()
+                .source(NO_DSP_NO_TRADES)
+                .first(DSP_NO_TRADES, dspApprovedGuard)
+                .then(NO_DSP_TRADES, tradesInFlightGuard)
+                .last(NO_DSP_NO_TRADES_DELAY)
+            .and().withChoice()
+                .source(DSP_NO_TRADES)
+                .first(EOD1_READY, tradesInFlightGuard)
+                .last(DSP_NO_TRADES_DELAY)
+            .and().withChoice()
+                .source(NO_DSP_TRADES)
+                .first(EOD1_READY, dspApprovedGuard)
+                .last(NO_DSP_TRADES_DELAY)
+            .and().withExternal()
+                .source(NO_DSP_TRADES_DELAY).target(NO_DSP_TRADES).timer(WAIT_TIME)
+            .and().withExternal()
+                .source(DSP_NO_TRADES_DELAY).target(DSP_NO_TRADES).timer(WAIT_TIME)
+            .and().withExternal()
+                .source(NO_DSP_NO_TRADES_DELAY).target(NO_DSP_NO_TRADES).timer(WAIT_TIME)
+            .and().withExternal()
+                .source(EOD1_READY).target(EOD1_RUN).action(eod1runAction)
+            .and().withChoice()
+                .source(EOD1_RUN)
+                .first(EOD1_COMPLETE, noErrorGuard())
+                .last(IDLE)
+            .and().withExternal()
+                .source(EOD1_COMPLETE).target(EOD2).action(eod1CompleteAction)
+            .and().withExternal()
+                .source(SWP_PNT_CHECK).target(SWP_PNT_NOTAPPROVED)
+            .and().withChoice()
+                .source(SWP_PNT_NOTAPPROVED)
+                .first(SWP_PNT_APPROVED, swpPointApprovedGuard)
+                .last(SWP_PNT_DELAY)
+            .and().withExternal()
+                .source(SWP_PNT_DELAY).target(SWP_PNT_NOTAPPROVED).timer(WAIT_TIME)
+            .and().withExternal()
+                .source(SWP_PNT_APPROVED).target(EOD2_RUN).action(eod2runAction)
+            .and().withChoice()
+                .source(EOD2_RUN)
+                .first(EOD2_COMPLETE, noErrorGuard())
+                .last(IDLE)
+            .and().withExternal()
+                .source(EOD2_COMPLETE).target(LEDGER_RUN).action(ledgerRunAction)
+            .and().withChoice()
+                .source(LEDGER_RUN)
+                .first(LEDGER_COMPLETE, noErrorGuard())
+                .last(IDLE)
+            .and().withExternal()
+                .source(LEDGER_COMPLETE).target(DATE_ROLL_RUN).action(dateRollRunAction)
+            .and().withExternal()
+                .source(DATE_ROLL_RUN).target(IDLE)
+        ;
+    }
+
+    @Bean
+    public Action<States, Events> eod1runAction() {
+        return new Action<>() {
+            @Override
+            public void execute(StateContext<States, Events> context) {
+                //context.getStateMachine().setStateMachineError(new RuntimeException("dddddddddddd"));
+            }
+        };
+    }
+
+    @Bean
+    public Action<States, Events> eod1CompleteAction() {
+        return new Action<>() {
+            @Override
+            public void execute(StateContext<States, Events> context) {
+                //context.getStateMachine().setStateMachineError(new RuntimeException("dddddddddddd"));
+            }
+        };
+    }
+
+    @Bean
+    public Action<States, Events> eod2runAction() {
+        return new Action<>() {
+            @Override
+            public void execute(StateContext<States, Events> context) {
+                //context.getStateMachine().setStateMachineError(new RuntimeException("dddddddddddd"));
+            }
+        };
+    }
+
+    @Bean
+    public Action<States, Events> ledgerRunAction() {
+        return new Action<>() {
+            @Override
+            public void execute(StateContext<States, Events> context) {
+                //context.getStateMachine().setStateMachineError(new RuntimeException("dddddddddddd"));
+            }
+        };
+    }
+
+    @Bean
+    public Action<States, Events> dateRollRunAction() {
+        return new Action<>() {
+            @Override
+            public void execute(StateContext<States, Events> context) {
+                //context.getStateMachine().setStateMachineError(new RuntimeException("dddddddddddd"));
+            }
+        };
+    }
+
+    @Bean
+    public Guard<States, Events> swpPointApprovedGuard() {
+        return new Guard<States, Events>() {
+
+            @Override
+            public boolean evaluate(StateContext<States, Events> context) {
+                return true;
+            }
+        };
+    }
+
+    @Bean
+    public Guard<States, Events> dspApprovedGuard() {
+        return new Guard<States, Events>() {
+
+            @Override
+            public boolean evaluate(StateContext<States, Events> context) {
+                return true;
+            }
+        };
+    }
+
+    @Bean
+    public Guard<States, Events> tradesInFlightGuard() {
+        return new Guard<States, Events>() {
+            @Override
+            public boolean evaluate(StateContext<States, Events> context) {
+                return false;
+            }
+        };
+    }
+
+    @Bean
+    public Guard<States, Events> noErrorGuard() {
+        return new Guard<States, Events>() {
+            @Override
+            public boolean evaluate(StateContext<States, Events> context) {
+                return !context.getStateMachine().hasStateMachineError();
+            }
+        };
+    }
+
+}
---
+++ src/test/java/com/ihsmarkit/tfx/eod/statemachine/StateMachineConfigTest.java
@@ -0,0 +1,111 @@
+package com.ihsmarkit.tfx.eod.statemachine;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.action.Action;
+import org.springframework.statemachine.guard.Guard;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@Import(StateMachineConfig.class)
+class StateMachineConfigTest {
+
+    @Autowired
+    private StateMachine<StateMachineConfig.States, StateMachineConfig.Events> stateMachine;
+
+    @MockBean(name = "dspApprovedGuard")
+    private Guard<StateMachineConfig.States, StateMachineConfig.Events> dspApprovedGuard;
+
+    @MockBean(name = "tradesInFlightGuard")
+    private Guard<StateMachineConfig.States, StateMachineConfig.Events> tradesInFlightGuard;
+
+    @MockBean(name = "swpPointApprovedGuard")
+    private Guard<StateMachineConfig.States, StateMachineConfig.Events> swpPointApprovedGuard;
+
+
+    @MockBean(name = "eod1runAction")
+    private Action<StateMachineConfig.States, StateMachineConfig.Events> eod1runAction;
+
+    @MockBean(name = "eod1CompleteAction")
+    private Action<StateMachineConfig.States, StateMachineConfig.Events> eod1CompleteAction;
+
+    @MockBean(name = "eod2runAction")
+    private Action<StateMachineConfig.States, StateMachineConfig.Events> eod2runAction;
+
+    @MockBean(name = "ledgerRunAction")
+    private Action<StateMachineConfig.States, StateMachineConfig.Events> ledgerRunAction;
+
+    @MockBean(name = "dateRollRunAction")
+    private Action<StateMachineConfig.States, StateMachineConfig.Events> dateRollRunAction;
+
+    @Before
+    void startMachine() {
+        stateMachine.start();
+    }
+
+    @After
+    void stopMachine() {
+        stateMachine.stop();
+    }
+
+    @Test
+    void shouldRunWhenDspApprovedFirst() throws Exception {
+        stateMachine.start();
+
+        when(dspApprovedGuard.evaluate(any())).thenReturn(false, false, true);
+        when(tradesInFlightGuard.evaluate(any())).thenReturn(false, false, false, true);
+        when(swpPointApprovedGuard.evaluate(any())).thenReturn(false, true);
+        stateMachine.sendEvent(StateMachineConfig.Events.EOD);
+
+        Thread.sleep(8000);
+
+        assertThat(stateMachine.getState().getId()).isEqualTo(StateMachineConfig.States.IDLE);
+        verify(dspApprovedGuard, times(3)).evaluate(any());
+        verify(tradesInFlightGuard, times(4)).evaluate(any());
+        verify(swpPointApprovedGuard, times(2)).evaluate(any());
+        stateMachine.stop();
+
+        verify(eod1runAction).execute(any());
+        verify(eod1CompleteAction).execute(any());
+        verify(eod2runAction).execute(any());
+        verify(ledgerRunAction).execute(any());
+        verify(dateRollRunAction).execute(any());
+    }
+
+    @Test
+    void shouldRunWhenNoTradesInFlightBeforeDspApproval() throws Exception {
+        stateMachine.start();
+
+        when(dspApprovedGuard.evaluate(any())).thenReturn(false, false, true);
+        when(tradesInFlightGuard.evaluate(any())).thenReturn(false, true);
+        when(swpPointApprovedGuard.evaluate(any())).thenReturn(true);
+        stateMachine.sendEvent(StateMachineConfig.Events.EOD);
+
+        Thread.sleep(6000);
+
+        assertThat(stateMachine.getState().getId()).isEqualTo(StateMachineConfig.States.IDLE);
+        verify(dspApprovedGuard, times(3)).evaluate(any());
+        verify(tradesInFlightGuard, times(2)).evaluate(any());
+        verify(swpPointApprovedGuard, times(1)).evaluate(any());
+        stateMachine.stop();
+
+        verify(eod1runAction).execute(any());
+        verify(eod1CompleteAction).execute(any());
+        verify(eod2runAction).execute(any());
+        verify(ledgerRunAction).execute(any());
+        verify(dateRollRunAction).execute(any());
+    }
+
+}
