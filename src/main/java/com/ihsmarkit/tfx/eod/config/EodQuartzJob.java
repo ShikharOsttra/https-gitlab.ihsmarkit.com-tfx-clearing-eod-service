347dfb06ac35ded2004c66fa5f2fad2749fb5f99
Yevgen Lirnyk <Yevgen.Lirnyk@ihsmarkit.com>
2020-01-09 16:56:40 +0200

    [TFX-558] integrating quartz

--- src/main/java/com/ihsmarkit/tfx/eod/config/EodQuartzJob.java
+++ src/main/java/com/ihsmarkit/tfx/eod/config/EodQuartzJob.java
@@ -1,54 +1,28 @@
 package com.ihsmarkit.tfx.eod.config;
 
-import static com.ihsmarkit.tfx.eod.config.EodJobConstants.BUSINESS_DATE_FMT;
-import static com.ihsmarkit.tfx.eod.config.EodJobConstants.BUSINESS_DATE_JOB_PARAM_NAME;
-import static com.ihsmarkit.tfx.eod.config.EodJobConstants.CURRENT_TSP_JOB_PARAM_NAME;
-import static com.ihsmarkit.tfx.eod.config.EodJobConstants.JOB_NAME_PARAM_NAME;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.Events.EOD;
 
 import org.quartz.JobExecutionContext;
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.configuration.JobLocator;
-import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.statemachine.StateMachine;
 
-import com.ihsmarkit.tfx.core.time.ClockService;
+import com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@SuppressFBWarnings("NP_NONNULL_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR")
+//@SuppressFBWarnings("NP_NONNULL_FIELD_NOT_INITIALIZED_IN_CONSTRUCTOR")
 @RequiredArgsConstructor
 public class EodQuartzJob extends QuartzJobBean {
 
-    private final JobLauncher jobLauncher;
-
-    private final JobLocator jobLocator;
-
-    private final ClockService clockService;
+    private StateMachine<StateMachineConfig.States, StateMachineConfig.Events> stateMachine;
 
     @Override
     @SneakyThrows
     protected void executeInternal(final JobExecutionContext context) {
-        final String jobName = context.getJobDetail().getJobDataMap().getString(JOB_NAME_PARAM_NAME);
-        final Job job = jobLocator.getJob(jobName);
-        final JobParameters params = new JobParametersBuilder()
-            .addString(BUSINESS_DATE_JOB_PARAM_NAME, getBusinessDate().format(BUSINESS_DATE_FMT))
-            .addString(CURRENT_TSP_JOB_PARAM_NAME, LocalDateTime.now().toString())
-            .toJobParameters();
-
-        jobLauncher.run(job, params);
+        stateMachine.sendEvent(EOD);
     }
 
-    private LocalDate getBusinessDate() {
-        return clockService.getCurrentDate().minus(1, ChronoUnit.DAYS);
-    }
 }
