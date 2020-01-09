a5a02f579feee34e9e51fdb20e84f75de5ae3493
Yevgen Lirnyk <Yevgen.Lirnyk@ihsmarkit.com>
2020-01-09 13:56:14 +0200

    [TFX-558] Fixed pipeline

--- src/test/java/com/ihsmarkit/tfx/eod/statemachine/StateMachineConfigTest.java
+++ src/test/java/com/ihsmarkit/tfx/eod/statemachine/StateMachineConfigTest.java
@@ -1,25 +1,32 @@
 package com.ihsmarkit.tfx.eod.statemachine;
 
+import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.READY;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.statemachine.StateContext;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.guard.Guard;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 @ExtendWith(SpringExtension.class)
 @Import(StateMachineConfig.class)
+@SuppressFBWarnings("MDM_THREAD_YIELD")
 class StateMachineConfigTest {
 
     @Autowired
@@ -52,19 +59,18 @@ class StateMachineConfigTest {
     @MockBean(name = "dateRollRunAction")
     private Action<StateMachineConfig.States, StateMachineConfig.Events> dateRollRunAction;
 
-    @Before
+    @BeforeEach
     void startMachine() {
         stateMachine.start();
     }
 
-    @After
+    @AfterEach
     void stopMachine() {
         stateMachine.stop();
     }
 
     @Test
     void shouldRunWhenDspApprovedFirst() throws Exception {
-        stateMachine.start();
 
         when(dspApprovedGuard.evaluate(any())).thenReturn(false, false, true);
         when(tradesInFlightGuard.evaluate(any())).thenReturn(false, false, false, true);
@@ -73,11 +79,10 @@ class StateMachineConfigTest {
 
         Thread.sleep(8000);
 
-        assertThat(stateMachine.getState().getId()).isEqualTo(StateMachineConfig.States.IDLE);
+        assertThat(stateMachine.getState().getId()).isEqualTo(READY);
         verify(dspApprovedGuard, times(3)).evaluate(any());
         verify(tradesInFlightGuard, times(4)).evaluate(any());
         verify(swpPointApprovedGuard, times(2)).evaluate(any());
-        stateMachine.stop();
 
         verify(eod1runAction).execute(any());
         verify(eod1CompleteAction).execute(any());
@@ -87,21 +92,40 @@ class StateMachineConfigTest {
     }
 
     @Test
+    void shouldReturnToIdleOnError() throws Exception {
+
+        when(dspApprovedGuard.evaluate(any())).thenReturn(true);
+        when(tradesInFlightGuard.evaluate(any())).thenReturn(true);
+        when(swpPointApprovedGuard.evaluate(any())).thenReturn(true);
+
+        doAnswer(invocation -> {
+            ((StateContext) invocation.getArgument(0)).getStateMachine().setStateMachineError(new RuntimeException());
+            return null;
+        }).when(eod1runAction).execute(any());
+
+        stateMachine.sendEvent(StateMachineConfig.Events.EOD);
+        Thread.sleep(500);
+
+        verify(eod1runAction).execute(any());
+        verifyNoMoreInteractions(eod1CompleteAction, eod2runAction, ledgerRunAction, dateRollRunAction);
+        assertThat(stateMachine.getState().getId()).isEqualTo(READY);
+
+    }
+
+    @Test
     void shouldRunWhenNoTradesInFlightBeforeDspApproval() throws Exception {
-        stateMachine.start();
 
         when(dspApprovedGuard.evaluate(any())).thenReturn(false, false, true);
         when(tradesInFlightGuard.evaluate(any())).thenReturn(false, true);
         when(swpPointApprovedGuard.evaluate(any())).thenReturn(true);
         stateMachine.sendEvent(StateMachineConfig.Events.EOD);
 
-        Thread.sleep(6000);
+        Thread.sleep(8000);
 
-        assertThat(stateMachine.getState().getId()).isEqualTo(StateMachineConfig.States.IDLE);
+        assertThat(stateMachine.getState().getId()).isEqualTo(READY);
         verify(dspApprovedGuard, times(3)).evaluate(any());
         verify(tradesInFlightGuard, times(2)).evaluate(any());
         verify(swpPointApprovedGuard, times(1)).evaluate(any());
-        stateMachine.stop();
 
         verify(eod1runAction).execute(any());
         verify(eod1CompleteAction).execute(any());
