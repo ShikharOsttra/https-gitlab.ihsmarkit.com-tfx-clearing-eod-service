e5097fc2c5d0d41479f1cc61c04201cccac9c10d
Yevgen Lirnyk <Yevgen.Lirnyk@ihsmarkit.com>
2020-01-14 18:41:26 +0200

    [TFX-558] cahcges per code review

--- src/test/java/com/ihsmarkit/tfx/eod/statemachine/StateWaitingListener.java
+++ src/test/java/com/ihsmarkit/tfx/eod/statemachine/StateWaitingListener.java
@@ -8,21 +8,19 @@ import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class StateWaitingListener extends StateMachineListenerAdapter<StateMachineConfig.States, StateMachineConfig.Events> {
     private final CountDownLatch latch = new CountDownLatch(1);
     private final StateMachineConfig.States state;
-
-    public StateWaitingListener(final StateMachineConfig.States state) {
-        this.state = state;
-    }
 
     @Override
     public void stateChanged(
         final State<StateMachineConfig.States, StateMachineConfig.Events> from,
         final State<StateMachineConfig.States, StateMachineConfig.Events> to
     ) {
-        if (latch.getCount() == 1L && to.getId() == state) {
+        if (to.getId() == state) {
             latch.countDown();
         }
     }
