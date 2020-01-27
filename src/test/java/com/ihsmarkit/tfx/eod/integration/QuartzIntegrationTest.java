69584679d20e5926a7f7baf59f5f1d5c7640de37
Yevgen Lirnyk <Yevgen.Lirnyk@ihsmarkit.com>
2020-01-27 16:23:17 +0200

    [TFX-1625] quartz schedule

--- src/test/java/com/ihsmarkit/tfx/eod/integration/QuartzIntegrationTest.java
+++ src/test/java/com/ihsmarkit/tfx/eod/integration/QuartzIntegrationTest.java
@@ -1,8 +1,17 @@
 package com.ihsmarkit.tfx.eod.integration;
 
+import static com.ihsmarkit.tfx.eod.config.EodJobConstants.CASH_BALANCE_UPDATE_BATCH_JOB_NAME;
 import static com.ihsmarkit.tfx.eod.config.EodJobConstants.EOD1_BATCH_JOB_NAME;
 import static com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig.States.EOD1;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,17 +27,24 @@ import org.springframework.statemachine.guard.Guard;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.ihsmarkit.tfx.core.dl.repository.SystemParameterRepository;
+import com.ihsmarkit.tfx.core.dl.repository.calendar.CalendarTradingSwapPointRepository;
+import com.ihsmarkit.tfx.eod.batch.CashCollateralBalanceUpdateTasklet;
+import com.ihsmarkit.tfx.eod.config.CashCollateralBalanceUpdateJobConfig;
 import com.ihsmarkit.tfx.eod.config.QuartzConfig;
 import com.ihsmarkit.tfx.eod.statemachine.StateMachineConfig;
 import com.ihsmarkit.tfx.eod.statemachine.StateWaitingListener;
 
 @ExtendWith(SpringExtension.class)
-@Import({IntegrationTestConfig.class, QuartzConfig.class, StateMachineConfig.class})
+@Import({IntegrationTestConfig.class, QuartzConfig.class, StateMachineConfig.class, CashCollateralBalanceUpdateJobConfig.class})
 @TestPropertySource(
     locations = "classpath:/application.properties",
-    properties = "eod1.job.enabled=true"
+    properties = {"eod1.job.enabled=true", "cashCollateralBalanceUpdate.job.enabled=true", "logging.level.org.quartz=DEBUG"}
 )
 public class QuartzIntegrationTest {
+
+    private static final LocalDate OCT_7 = LocalDate.of(2019, 10, 7);
+
     @Autowired
     private StateMachine<StateMachineConfig.States, StateMachineConfig.Events> stateMachine;
 
@@ -59,6 +75,15 @@ public class QuartzIntegrationTest {
     @MockBean(name = "dateRollRunAction")
     private Action<StateMachineConfig.States, StateMachineConfig.Events> dateRollRunAction;
 
+    @MockBean
+    private SystemParameterRepository systemParameterRepository;
+
+    @MockBean
+    private CalendarTradingSwapPointRepository calendarTradingSwapPointRepository;
+
+    @MockBean
+    private CashCollateralBalanceUpdateTasklet cashCollateralBalanceUpdateTasklet;
+
     @Autowired
     private Scheduler scheduler;
 
@@ -69,6 +94,25 @@ public class QuartzIntegrationTest {
         scheduler.triggerJob(new JobKey(EOD1_BATCH_JOB_NAME, null));
         listener.await(stateMachine, 5);
         assertThat(stateMachine.getState().getId()).isEqualTo(EOD1);
+    }
+
+    @Test
+    void shouldTriggerBalanceUpdateExection() throws Exception {
+        when(systemParameterRepository.getParameterValueFailFast(any())).thenReturn(OCT_7);
+        when(calendarTradingSwapPointRepository.findPreviousTradingDate(any())).thenReturn(Optional.of(OCT_7));
+
+        final CountDownLatch finished = new CountDownLatch(1);
+
+        doAnswer(
+            invocation -> {
+                finished.countDown();
+                return null;
+            }
+        ).when(cashCollateralBalanceUpdateTasklet).execute(any(), any());
+
+        scheduler.triggerJob(new JobKey(CASH_BALANCE_UPDATE_BATCH_JOB_NAME, null));
+
+        assertThat(finished.await(5, TimeUnit.SECONDS)).isTrue();
     }
 
 }
