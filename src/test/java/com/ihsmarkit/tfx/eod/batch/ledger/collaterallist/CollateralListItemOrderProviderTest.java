b310708a1bb0d86210376eafa6bbea570e8ee112
Andrii Korovin <Andrii_Korovin@epam.com>
2020-03-12 16:43:45 +0200

    [TFX-2916] improve ledger D sorting

--- src/test/java/com/ihsmarkit/tfx/eod/batch/ledger/collaterallist/CollateralListItemOrderProviderTest.java
+++ src/test/java/com/ihsmarkit/tfx/eod/batch/ledger/collaterallist/CollateralListItemOrderProviderTest.java
@@ -1,12 +1,22 @@
 package com.ihsmarkit.tfx.eod.batch.ledger.collaterallist;
 
+import static com.ihsmarkit.tfx.core.dl.CollateralTestDataFactory.aBondCollateralProductEntityBuilder;
+import static com.ihsmarkit.tfx.core.dl.CollateralTestDataFactory.aCashCollateralProductEntityBuilder;
 import static com.ihsmarkit.tfx.core.dl.CollateralTestDataFactory.aCollateralBalanceEntityBuilder;
+import static com.ihsmarkit.tfx.core.dl.CollateralTestDataFactory.aLogCollateralProductEntityBuilder;
+import static com.ihsmarkit.tfx.core.dl.CollateralTestDataFactory.anEquityCollateralProductEntityBuilder;
+import static com.ihsmarkit.tfx.core.dl.EntityTestDataFactory.anIssuerBankEntityBuilder;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
+
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -29,15 +39,40 @@ class CollateralListItemOrderProviderTest {
         when(participantCodeOrderIdProvider.get("BNP")).thenReturn(5);
     }
 
-    @Test
-    void shouldProvideOrderIdForOrdinaryRow() {
-        final CollateralBalanceEntity collateralBalanceEntity = aCollateralBalanceEntityBuilder().build();
-        assertThat(collateralListItemOrderProvider.getOrderId(collateralBalanceEntity, LedgerConstants.ITEM_RECORD_TYPE)).isEqualTo(5113L);
+    @ParameterizedTest
+    @MethodSource("orderIdForOrdinaryRowDataProvider")
+    void shouldProvideOrderIdForOrdinaryRow(final CollateralBalanceEntity balance, final long expectedOrderId) {
+        assertThat(collateralListItemOrderProvider.getOrderId(balance, LedgerConstants.ITEM_RECORD_TYPE)).isEqualTo(expectedOrderId);
+    }
+
+    private static Stream<Arguments> orderIdForOrdinaryRowDataProvider() {
+        return Stream.of(
+            Arguments.of(aCollateralBalanceEntityBuilder()
+                .product(aLogCollateralProductEntityBuilder()
+                    .issuer(anIssuerBankEntityBuilder()
+                        .subType(10)
+                        .build())
+                    .build()
+                ).build(), 51120000000010L),
+            Arguments.of(aCollateralBalanceEntityBuilder()
+                .product(aBondCollateralProductEntityBuilder()
+                    .securityCode("123456789")
+                    .build())
+                .build(), 51130123456789L),
+            Arguments.of(aCollateralBalanceEntityBuilder()
+                .product(anEquityCollateralProductEntityBuilder()
+                    .securityCode("000000789")
+                    .build())
+                .build(), 51140000000789L),
+            Arguments.of(aCollateralBalanceEntityBuilder()
+                .product(aCashCollateralProductEntityBuilder().build())
+                .build(), 51110000000000L)
+        );
     }
 
     @Test
     void shouldProvideOrderIdForTotalRow() {
         final CollateralListItemTotalKey collateralListItemTotalKey = CollateralListItemTotalKey.of("BNP", "8");
-        assertThat(collateralListItemOrderProvider.getOrderId(collateralListItemTotalKey, LedgerConstants.SUBTOTAL_RECORD_TYPE)).isEqualTo(5840L);
+        assertThat(collateralListItemOrderProvider.getOrderId(collateralListItemTotalKey, LedgerConstants.SUBTOTAL_RECORD_TYPE)).isEqualTo(58400000000000L);
     }
 }
