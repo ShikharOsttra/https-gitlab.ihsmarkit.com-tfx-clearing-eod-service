2a47c2442e4badc672be3a7f04d1dc6cfa5132b8
Andrii Korovin <Andrii_Korovin@epam.com>
2020-04-06 11:43:25 +0300

    [TFX-3092] extract comparator to OrderUtils

--- src/main/java/com/ihsmarkit/tfx/eod/batch/ledger/ParticipantCodeOrderIdProvider.java
+++ src/main/java/com/ihsmarkit/tfx/eod/batch/ledger/ParticipantCodeOrderIdProvider.java
@@ -1,8 +1,8 @@
 package com.ihsmarkit.tfx.eod.batch.ledger;
 
 import static com.ihsmarkit.tfx.core.domain.Participant.CLEARING_HOUSE_CODE;
+import static com.ihsmarkit.tfx.eod.batch.ledger.OrderUtils.matchingComparator;
 
-import java.util.Comparator;
 import java.util.stream.Stream;
 
 import org.springframework.batch.core.configuration.annotation.StepScope;
@@ -21,17 +21,7 @@ public class ParticipantCodeOrderIdProvider extends OrderIdProvider {
 
     @Override
     public Stream<String> loadDataStream() {
-        return participantRepository.findAllCodes().stream().sorted(clhsLast());
-    }
-
-    private static Comparator<String> clhsLast() {
-        return (code1, code2) -> {
-            if (CLEARING_HOUSE_CODE.equals(code1)) {
-                return CLEARING_HOUSE_CODE.equals(code2) ? 0 : 1;
-            } else {
-                return CLEARING_HOUSE_CODE.equals(code2) ? -1 : code1.compareTo(code2);
-            }
-        };
+        return participantRepository.findAllCodes().stream().sorted(matchingComparator(CLEARING_HOUSE_CODE::equals, false));
     }
 
 }
