470b30a69ebc036848bf6e7fb9d4c62faf8ea534
Andrii Korovin <Andrii_Korovin@epam.com>
2020-04-21 17:14:25 +0300

    [TFX-2496] same record date for all rows in transaction diary ledger

--- src/main/java/com/ihsmarkit/tfx/eod/config/EodJobConstants.java
+++ src/main/java/com/ihsmarkit/tfx/eod/config/EodJobConstants.java
@@ -34,6 +34,7 @@ public final class EodJobConstants {
     public static final String OPEN_POSITIONS_LEDGER_STEP_NAME = "openPositionsLedger";
 
     public static final String COLLATERAL_BALANCE_LEDGER_STEP_NAME = "collateralBalanceLedger";
+    public static final String TRANSACTION_DIARY_RECORD_DATE_SET_STEP_NAME = "setTransactionDiaryRecordDateStep";
     public static final String TRADE_TRANSACTION_DIARY_LEDGER_STEP_NAME = "tradeTransactionDiaryLedger";
     public static final String SOD_TRANSACTION_DIARY_LEDGER_STEP_NAME = "sodTransactionDiaryLedger";
     public static final String NET_TRANSACTION_DIARY_LEDGER_STEP_NAME = "netTransactionDiaryLedger";
