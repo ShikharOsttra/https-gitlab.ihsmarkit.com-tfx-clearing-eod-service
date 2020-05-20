333bf5a54dd8ac4d3a7d7109b0024f42bf1ed507
Andrii Korovin <Andrii_Korovin@epam.com>
2020-05-20 23:11:38 +0300

    [TFX-2567] EOD uses collateral requirement by next business date instead of current business date

--- src/main/java/com/ihsmarkit/tfx/eod/batch/ledger/collateralbalance/CollateralRequirementProvider.java
+++ src/main/java/com/ihsmarkit/tfx/eod/batch/ledger/collateralbalance/CollateralRequirementProvider.java
@@ -1,11 +1,17 @@
 package com.ihsmarkit.tfx.eod.batch.ledger.collateralbalance;
 
+import static com.google.common.collect.Tables.transformValues;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toMap;
+
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
 
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,6 +25,7 @@ import com.google.common.collect.Tables;
 import com.ihsmarkit.tfx.core.dl.entity.collateral.ParticipantCollateralRequirementEntity;
 import com.ihsmarkit.tfx.core.dl.repository.collateral.ParticipantCollateralRequirementRepository;
 import com.ihsmarkit.tfx.core.domain.type.CollateralPurpose;
+import com.ihsmarkit.tfx.eod.service.CalendarDatesProvider;
 
 import lombok.AllArgsConstructor;
 
@@ -32,24 +39,34 @@ public class CollateralRequirementProvider {
 
     private final ParticipantCollateralRequirementRepository requirementRepository;
 
+    private final CalendarDatesProvider calendarDatesProvider;
+
     private final Lazy<Table<Long, CollateralPurpose, BigDecimal>> requiredAmounts = Lazy.of(this::loadRequiredAmount);
 
     private final Lazy<Map<Long, Pair<LocalDate, BigDecimal>>> nextClearingDepositRequiredAmount = Lazy.of(this::loadNextClearingDepositRequiredAmount);
 
     private Table<Long, CollateralPurpose, BigDecimal> loadRequiredAmount() {
 
-        return requirementRepository.findByBusinessDate(businessDate).stream()
-            .collect(Tables.toTable(
-                requiredAmount -> requiredAmount.getParticipant().getId(),
-                ParticipantCollateralRequirementEntity::getPurpose,
-                requirementEntity -> requirementEntity.getValue().setScale(0, RoundingMode.DOWN),
-                HashBasedTable::create));
+        return requirementRepository.findNotOutdatedByBusinessDate(businessDate).stream()
+            .filter(requiredAmount -> !requiredAmount.getBusinessDate().isAfter(calendarDatesProvider.getNextBusinessDate()))
+            .collect(
+                collectingAndThen(
+                    Tables.toTable(
+                        requiredAmount -> requiredAmount.getParticipant().getId(),
+                        ParticipantCollateralRequirementEntity::getPurpose,
+                        Function.identity(),
+                        BinaryOperator.maxBy(Comparator.comparing(ParticipantCollateralRequirementEntity::getBusinessDate)),
+                        HashBasedTable::create
+                    ),
+                    table -> transformValues(table, requiredAmount -> requiredAmount.getValue().setScale(0, RoundingMode.DOWN))
+                )
+            );
     }
 
     private Map<Long, Pair<LocalDate, BigDecimal>> loadNextClearingDepositRequiredAmount() {
-        return requirementRepository.findFutureOnlyByBusinessDate(businessDate).stream()
+        return requirementRepository.findFutureOnlyByBusinessDate(calendarDatesProvider.getNextBusinessDate()).stream()
             .filter(requirement -> requirement.getPurpose() == CollateralPurpose.CLEARING_DEPOSIT)
-            .collect(Collectors.toMap(
+            .collect(toMap(
                 requiredAmount -> requiredAmount.getParticipant().getId(),
                 requiredAmount -> Pair.of(requiredAmount.getApplicableDate(), requiredAmount.getValue().setScale(0, RoundingMode.DOWN))
             ));
