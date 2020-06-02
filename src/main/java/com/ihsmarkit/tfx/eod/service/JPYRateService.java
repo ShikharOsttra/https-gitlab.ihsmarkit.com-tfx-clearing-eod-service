722c440ae6d08a7f958970321a29e813ea2add15
Yauheni Maltsau <yauheni.maltsau@ihsmarkit.com>
2020-06-02 18:46:21 +0100

    [TFX-0000] exception handling

--- src/main/java/com/ihsmarkit/tfx/eod/service/JPYRateService.java
+++ src/main/java/com/ihsmarkit/tfx/eod/service/JPYRateService.java
@@ -6,23 +6,29 @@ import static com.ihsmarkit.tfx.eod.config.EodJobConstants.JPY;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Optional;
 
 import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @JobScope
 @RequiredArgsConstructor
+@Slf4j
 public class JPYRateService {
 
     private final DailySettlementPriceService dailySettlementPriceService;
 
     @Cacheable(JPY_RATES_CACHE)
     public BigDecimal getJpyRate(final LocalDate date, final String currency) {
-        return dailySettlementPriceService.getPrice(date, currency, JPY);
+        return Optional.ofNullable(dailySettlementPriceService.getPrice(date, currency, JPY)).orElseThrow(() -> {
+            log.warn("unable to find JPY price for currency: {} on date: {}", currency, date);
+            return new IllegalArgumentException("unable to find JPY price");
+        });
     }
 
     @Cacheable(JPY_CROSS_RATES_CACHE)
